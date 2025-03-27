4c0b76b87f6a649383b0b81bf26b446a67f97a2c
vivekkothari <vivekkothari2304@gmail.com>
2025-03-27 14:09:42 +0530

    Move to cache package

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	cache2 "github.com/vivekkothari/in-memory-cache/cache"
 	"time"
 )
 
@@ -13,7 +14,7 @@ func main() {
 		return "", false
 	}
 
-	var cache Cache[string, string] = NewLRUCache[string, string](2, 5*time.Second, backingStore, nil, 5*time.Second)
+	var cache cache2.Cache[string, string] = cache2.NewLRUCache[string, string](2, 5*time.Second, backingStore, nil, 5*time.Second)
 
 	cache.Put("key1", "value1")
 	fmt.Println(cache.Get("key1")) // Expected: value1
